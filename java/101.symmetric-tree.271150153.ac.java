533658f384c0d7d6ab6b22d6759c26a7bf2cd8ec
TangliziGit <tanglizimail@foxmail.com>
2019-10-19 12:15:23 +0800

    complete #101 symmetric-tree in java

---
+++ java/101.symmetric-tree.271150153.ac.java
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) return true;
+        return compare(root.left, root.right);
+    }
+    
+    private boolean compare(TreeNode l, TreeNode r){
+        if (l == null && r == null) return true;
+        if (l == null || r == null) return false;
+        if (l.val != r.val) return false;
+        return compare(l.left, r.right) && compare(l.right, r.left);
+    }
+}
